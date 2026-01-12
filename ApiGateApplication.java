4569078afd4fbdae8d1549ec04c51866ce211650
Garcia Labastida Miguel Angel <miguel.angel.garcia.labastida@outlook.com>
2026-01-12 16:59:54 -0600

    Create ApiGateApplication.java

---
+++ ApiGateApplication.java
@@ -0,0 +1,11 @@
+package com.tuorg.apigate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGateApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(ApiGateApplication.class, args);
+  }
+}
